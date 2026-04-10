e8889b01e1e5ab284c0de1e319ffa53a439a0a37
Syed Muneeb Ahmad Bukhari <muneebbukhari@Syeds-MacBook-Pro.local>
2026-04-10 22:07:48 +0100

    updating raw data analysis

--- analysis.py
+++ analysis.py
@@ -1,37 +1,3 @@
-# import pandas as pd
-
-# df = pd.read_csv("github_data.csv")
-
-# # Repository Age Analysis
-# df["created_at"] = pd.to_datetime(df["created_at"], utc=True)
-# df["updated_at"] = pd.to_datetime(df["updated_at"], utc=True)
-# df["age_days"] = (pd.Timestamp.now(tz="UTC") - df["created_at"]).dt.days
-
-# # Handle missing language
-# df["language"] = df["language"].fillna("Unknown")
-# print(df.describe())
-# print(df["language"].value_counts())
-
-
-
-
-# df["stars_per_day"] = df["stars"] / df["age_days"]
-# df["forks_per_star"] = df["forks"] / df["stars"]
-# df["contributors_per_star"] = df["contributors"] / df["stars"]
-
-# df.sort_values("stars", ascending=False)[["name", "stars"]]
-
-# ### Scenario:1 Popularity Visualization
-# df["stars"].plot(kind="bar", title="Stars per Repository")
-# plt.show()
-
-# # Scatter plot
-# plt.scatter(df["forks"], df["stars"])
-# plt.xlabel("Forks")
-# plt.ylabel("Stars")
-# plt.title("Forks vs Stars")
-# plt.show()
-
 from db import get_collection
 import pandas as pd
 
